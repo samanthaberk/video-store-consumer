aea7ee8e29a5dbdbd2fdf4ad26354c74d8db9f81
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-06-18 15:29:23 -0700

    List of all customers displays on customer list page.

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CustomerList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      customers: []
+    }
+  }
+
+  componentDidMount() {
+    const CUSTOMERS_URL = 'http://localhost:3000' + '/customers';
+    axios.get(CUSTOMERS_URL)
+      .then((response) => {
+        // console.log(response);
+        let updatedCustomers = []; response.data.map((customer) => {
+          updatedCustomers.push(customer);
+        });
+        this.setState({
+          customers: updatedCustomers
+        });
+      })
+      .catch();
+  }
+
   render() {
+    const customers = this.state.customers.map((customer) => {
+      return (<p>{customer.name}</p>);
+    });
     return (
+      <div>
       <h1>CustomerList!</h1>
+      {customers}
+      </div>
     );
   }
 }
