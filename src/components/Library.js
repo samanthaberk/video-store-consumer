d27ec528253b1e6eed5aab5190a8741a5eacd2e1
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-06-19 09:59:01 -0700

    Display movies' image and title in library page.

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Library extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    const LIBRARY_URL = 'http://localhost:3000' + '/movies';
+    axios.get(LIBRARY_URL)
+      .then((response) => {
+        let updatedMovies = []
+        response.data.map((movie) => {
+          updatedMovies.push(movie);
+        });
+        this.setState({
+          movies: updatedMovies
+        });
+      })
+      .catch();
+  }
+
   render() {
+    const movies = this.state.movies.map((movie) => {
+      return (
+        <div key={movie.id}>
+          <p>
+            <img src={movie.image_url} />
+            {movie.title}
+            <button
+              value={movie.id}
+              name={movie.title}
+            >Select Movie</button>
+          </p>
+        </div>
+      );
+    });
     return (
-      <h1>Library!</h1>
+      <div>
+        <h1>Library!</h1>
+        {movies}
+      </div>
     );
   }
 }
