61759ce80f833c85b305da8016d508f51118a9b5
Samantha Berk <sberk10@gmail.com>
2018-06-19 11:03:31 -0700

    Movie Search form displays list of movies according to search term

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Search extends Component {
   constructor() {
     super();
     this.state = {
-      searchTerm: ""
+      searchTerm: "",
+      results: []
     }
   }
 
@@ -14,11 +16,42 @@ class Search extends Component {
     this.setState(updatedSearchTerm);
   }
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    const SEARCH_URL = `http://localhost:3000/movies?query=${this.state.searchTerm}`;
+    axios.get(SEARCH_URL)
+    .then((response) => {
+      let updatedResults = [];
+      response.data.map((result) => {
+        updatedResults.push(result)
+      });
+      this.setState({
+        results: updatedResults,
+        searchTerm: ""
+      });
+    })
+    .catch();
+  }
+
   render() {
     console.log(this.state.searchTerm);
+    const results = this.state.results.map((movie) => {
+      return(
+        <div key={movie.external_id}>
+          <p>
+            <img src={movie.image_url} />
+            {movie.title}
+            <button
+              value={movie.external_id}
+              name={movie.title}
+            >Add Movie to Library</button>
+          </p>
+        </div>
+      );
+    });
     return (
       <section>
-        <form>
+        <form onSubmit={this.onFormSubmit}>
           <input
             onChange={this.onInputChange}
             type="text"
@@ -26,6 +59,11 @@ class Search extends Component {
           />
           <input type="submit" />
         </form>
+
+        <div className='result'>
+          {results}
+        </div>
+
       </section>
     );
   }
